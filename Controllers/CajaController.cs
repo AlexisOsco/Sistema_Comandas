a638d395fb3812d83a1373ce4c76d2f91ead0467
AlexisOsco <yampol.osco@gmail.com>
2024-12-01 13:53:56 -0500

    Creacion de las vistas

---
+++ Controllers/CajaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_Comandas.Controllers
+{
+    public class CajaController : Controller
+    {
+        public IActionResult Caja()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ListaPreciosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_Comandas.Controllers
+{
+    public class ListaPreciosController : Controller
+    {
+        public IActionResult ListaPrecios()
+        {
+            return View();
+        }
+    }
+}
